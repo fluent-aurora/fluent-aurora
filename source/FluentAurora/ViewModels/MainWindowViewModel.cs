0fdc781b40b3a98a014f8b44004d7b197ce5f98f
Shazzaam <78475878+shazzaam7@users.noreply.github.com>
2025-10-16 10:25:58 +0200

    refactor: Added multiple different `MinHeight` values
    This depends on if `ExpandedPlayback` is enabled or not

--- source/FluentAurora/ViewModels/MainWindowViewModel.cs
+++ source/FluentAurora/ViewModels/MainWindowViewModel.cs
@@ -1,13 +1,22 @@
-﻿using FluentAurora.Services;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using FluentAurora.Services;
 
 namespace FluentAurora.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
     public PlaybackControlService PlaybackControlService { get; }
+    public double MinWindowHeight => PlaybackControlService.IsExpanded ? 935 : 100;
 
     public MainWindowViewModel(PlaybackControlService playbackControlService)
     {
         PlaybackControlService = playbackControlService;
+        PlaybackControlService.PropertyChanged += (s, e) =>
+        {
+            if (e.PropertyName == nameof(PlaybackControlService.IsExpanded))
+            {
+                OnPropertyChanged(nameof(MinWindowHeight));
+            }
+        };
     }
 }

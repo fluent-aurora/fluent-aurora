6c1d57f9030b3cfa74abb385098b42ed339eb2f4
Shazzaam <78475878+shazzaam7@users.noreply.github.com>
2025-10-16 10:33:18 +0200

    refactor: Improved `ExpandedPlaybackControl` UI and smaller `MinHeight`

--- source/FluentAurora/ViewModels/MainWindowViewModel.cs
+++ source/FluentAurora/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using FluentAurora.Services;
 
 namespace FluentAurora.ViewModels;
@@ -6,7 +7,7 @@ namespace FluentAurora.ViewModels;
 public partial class MainWindowViewModel : ViewModelBase
 {
     public PlaybackControlService PlaybackControlService { get; }
-    public double MinWindowHeight => PlaybackControlService.IsExpanded ? 935 : 100;
+    public double MinWindowHeight => PlaybackControlService.IsExpanded ? 885 : 110;
 
     public MainWindowViewModel(PlaybackControlService playbackControlService)
     {

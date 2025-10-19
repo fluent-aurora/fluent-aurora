030ebad409f49d6a4ae16956462fcc77d9158c22
Shazzaam <78475878+shazzaam7@users.noreply.github.com>
2025-10-19 11:07:00 +0200

    refactor: Improved `PositionToTimeConverter`
    Now it supports `int`, `double`, `float` & `long`

--- source/FluentAurora/Converters/PositionToTimeConverter.cs
+++ source/FluentAurora/Converters/PositionToTimeConverter.cs
@@ -7,52 +7,68 @@ namespace FluentAurora.Converters;
 
 public class PositionToTimeConverter : IValueConverter, IMultiValueConverter
 {
-    // Properties
+    // Singleton instance
     public static readonly PositionToTimeConverter Instance = new PositionToTimeConverter();
 
-    // Methods
     /// <summary>
-    /// Formats the value (in seconds) to a format (hours\minutes\seconds)
+    /// Formats seconds to h:mm:ss or mm:ss
     /// </summary>
-    /// <param name="seconds">Value (in milliseconds)</param>
-    /// <returns>Formatted value in hours\minutes\seconds format</returns>
     private static string Format(double seconds) => TimeSpan.FromSeconds(seconds).ToString(seconds >= 3600 ? @"h\:mm\:ss" : @"mm\:ss");
 
     /// <summary>
-    /// Single value conversion
+    /// Helper to convert any numeric type to double
     /// </summary>
-    /// <param name="value">Value (in milliseconds)</param>
-    /// <param name="targetType"></param>
-    /// <param name="parameter"></param>
-    /// <param name="culture"></param>
-    /// <returns>Formated Value if it is present, otherwise an empty string</returns>
-    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    private static bool TryConvertToDouble(object? value, out double result)
     {
-        if (value is not int currentMs)
+        switch (value)
         {
-            return string.Empty;
+            case int i:
+                result = i;
+                return true;
+            case double d:
+                result = d;
+                return true;
+            case float f:
+                result = f;
+                return true;
+            case long l:
+                result = l;
+                return true;
+            default:
+                result = 0;
+                return false;
         }
-        double currentSec = currentMs / 1000.0;
-        return $"{Format(currentSec)}";
     }
 
     /// <summary>
-    /// Multi-value conversion
+    /// Single value conversion (milliseconds)
     /// </summary>
-    /// <param name="values">Values (in milliseconds)</param>
-    /// <param name="targetType"></param>
-    /// <param name="parameter"></param>
-    /// <param name="culture"></param>
-    /// <returns>Formated values (Current Position/Song Duration) if values are present, otherwise an empty string</returns>
-    public object Convert(IList<object?> values, Type targetType, object? parameter, CultureInfo culture)
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
-        if (values is not [int currentMs, int durationMs])
+        if (!TryConvertToDouble(value, out double ms))
         {
             return string.Empty;
         }
-        double currentSec = currentMs / 1000.0;
-        double durationSec = durationMs / 1000.0;
-        return $"{Format(currentSec)} / {Format(durationSec)}";
+
+        return Format(ms / 1000.0);
+    }
+
+    /// <summary>
+    /// Multi-value conversion: current / duration
+    /// </summary>
+    public object Convert(IList<object?> values, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (values == null || values.Count != 2)
+        {
+            return string.Empty;
+        }
+
+        if (!TryConvertToDouble(values[0], out double currentMs) || !TryConvertToDouble(values[1], out double durationMs))
+        {
+            return string.Empty;
+        }
+
+        return $"{Format(currentMs / 1000.0)} / {Format(durationMs / 1000.0)}";
     }
 
     public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) => throw new NotImplementedException();
